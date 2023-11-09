f35dd823695cc42f869124683c8d66a4397d22cc
lapinRagnar <lapinragnar@gmail.com>
2023-11-09 15:38:57 +0100

    onAuthStateChanged - verifier si l'user existe

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,17 +1,29 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth'
 
 import {auth} from '../firebase'
 
 const UserContext = createContext(null)
 
-console.log("auth", auth)
 
 export const AuthContextProvider = ({children}) => {
+
+  const [user, setUser] = useState({})
   
   const createUser = (user, password) => {
     return createUserWithEmailAndPassword(auth, user, password)
   }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser)
+      setUser(currentUser)
+      
+      return () => {
+        unsubscribe()
+      }
+    })
+  }, [])
 
 
   return (
